82ab397ff9afd02e8570a20a62f9a7cf38807592
LAPTOP-8LRM34JR\orolo <bt95240@gmail.com>
2022-01-11 12:06:49 +0100

    manifest defectueux

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import logo from './Logo.png';
 import './App.css';
+import '../src/components/main';
+import Main from '../src/components/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Main/>
+    </>
   );
 }
 
