0fe5dc6aef9fc2ffd3dbf99b8f99ce249bc44aa5
celinelouaintier <celine.louaintier@gmail.com>
2022-01-11 16:52:59 +0100

    Galère API

--- main/src/App.js
+++ main/src/App.js
@@ -2,11 +2,13 @@ import logo from './Logo.png';
 import './App.css';
 import '../src/components/main';
 import Main from '../src/components/main';
+import Grammaire from './components/grammaire';
 
 function App() {
   return (
     <>
     <Main/>
+    <Grammaire/>
     </>
   );
 }
